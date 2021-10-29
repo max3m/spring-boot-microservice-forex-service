baad053f07d4cd64015ca18963942009b39c29b0
max3m <max-samara120@mail.ru>
2021-10-30 00:14:44 +0300

    added eureka

---
+++ src/main/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/model/ExchangeValue.java
@@ -0,0 +1,39 @@
+package com.in28minutes.springboot.microservice.example.forex.springbootmicroserviceforexservice.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class ExchangeValue {
+
+    @Id
+    private Long id;
+
+    @Column(name = "currency_from")
+    private String from;
+
+    @Column(name = "currency_to")
+    private String to;
+
+    private BigDecimal conversionMultiple;
+
+    @Setter
+    private int port;
+
+    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+        super();
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+}
